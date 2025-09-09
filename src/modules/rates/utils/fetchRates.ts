eece06e36c580ed29e8d63966a86a8d3bd6212b0
Hleb <hlebor2006@mail.ru>
2025-09-09 19:19:57 +0300

    apiHost was created

--- src/modules/rates/utils/fetchRates.ts
+++ src/modules/rates/utils/fetchRates.ts
@@ -2,6 +2,7 @@ import { InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios, { AxiosResponse } from 'axios';
 import { RatesResponseData } from '../types/ResponseData';
+import { apiHost } from 'src/constants/currency-api.constants';
 
 export default async function fetchRates(
   configService: ConfigService,
@@ -9,9 +10,15 @@ export default async function fetchRates(
   query_target: string,
 ): Promise<AxiosResponse<RatesResponseData>> {
   try {
-    return await axios.get(
-      `https://api.fxratesapi.com/latest?api_key=${configService.get('fxRatesApiKey')!}&base=${base_currency}&currencies=${query_target}&resolution=1d&format=json`,
-    );
+    const urlSearchParams = new URLSearchParams();
+
+    urlSearchParams.set('api_key', configService.get('fxRatesApiKey')!);
+    urlSearchParams.set('base', base_currency);
+    urlSearchParams.set('currencies', query_target);
+    urlSearchParams.set('resolution', '1d');
+    urlSearchParams.set('format', 'json');
+
+    return await axios.get(`${apiHost}/latest?${urlSearchParams.toString()}`);
   } catch {
     throw new InternalServerErrorException('Cannot fetch rates');
   }
